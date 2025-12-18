cd6a4be83fed9e0f833fd6e403c69d2ccf83237a
Macar Angelo A. Butones <75861463+djdarkblazer@users.noreply.github.com>
2025-12-18 23:09:26 +0800

    Delete preload.js

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('patcher', {
-  browsePath: () => ipcRenderer.invoke('browse-path'),
-  checkUpdates: (targetPath) => ipcRenderer.invoke('check-updates', targetPath),
-  patchFiles: (updates, targetPath) => ipcRenderer.invoke('patch-files', updates, targetPath),
-  onDownloadProgress: (callback) => ipcRenderer.on('download-progress', (_, data) => callback(data)),
-  onFileCompleted: (callback) => ipcRenderer.on('file-completed', (_, data) => callback(data))
-});
